f8162e7c1d176c22f605bda9befb48597312a459
shantanu pandey <shantanupandey@shantanus-MacBook.local>
2021-05-11 16:23:18 +0530

    Increase and Decrease Quantity setState added

--- src/CartItem.js
+++ src/CartItem.js
@@ -17,8 +17,47 @@ class CartItem extends React.Component {
     }
 
     increaseQuantity = () => {
-        console.log(this);
+        //console.log(this);
+        //setState form 1
+        //calling setState we can trigger a rerender our component with the updated value
+        //passing it an object with what to change
+        // React will take this object and it will merge it with the state object => Shallow Merging
+        //If I want to change the quantity, then it will only touch the qty and nothing else
+        // this.setState({
+        //     qty: this.state.qty + 1
+        // });
+
+
+        //setState form 2 - IF PREVIOUS STATE REQUIRED
+
+        this.setState((prevState) => {
+            return {
+                qty: prevState.qty + 1
+            }
+
+        }, () => {
+            console.log(this.state)
+        });
+
     }
+
+    decreaseQuantity = () => {
+
+        const {qty} = this.state
+
+        if (qty === 0){
+            return;
+        }
+
+        this.setState((prevState) => {
+            return {
+                
+                qty: prevState.qty - 1
+            }
+        });
+    }
+
+    
     render(){
         //method returns JSX that describes the UI for the component
 
@@ -48,6 +87,7 @@ class CartItem extends React.Component {
                         alt="decrease" 
                         className="action-icons" 
                         src="https://www.flaticon.com/svg/vstatic/svg/992/992683.svg?token=exp=1620664673~hmac=5a7969abe7d44e91271e4eb88679ea10" 
+                        onClick={this.decreaseQuantity}
                         />
                         <img 
                         alt="delete" 
